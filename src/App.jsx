c7dae0aef5a5400bc863ca686f7c106914f7312b
diptowebhero <diptodas1672@gmail.com>
2023-06-30 18:55:16 +0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 className="min-h-screen flex items-center justify-center text-purple-500 font-bold text-5xl">Welcome to Bistro Boss Restaurant</h1>
+    </>
+  );
+}
+
+export default App;
