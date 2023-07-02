8c508b23ea1ca1c0a636ca0281ee8212c603e08b
diptowebhero <diptodas1672@gmail.com>
2023-07-02 23:38:15 +0600

    categories section

--- src/components/Home/Categories/Categories.jsx
+++ src/components/Home/Categories/Categories.jsx
@@ -1,11 +1,102 @@
+import SwiperCore, { EffectCoverflow, Pagination } from "swiper/core";
+import { Swiper, SwiperSlide } from "swiper/react";
+import img2 from "./../../../assets/home/slide-2.jpg";
+import img3 from "./../../../assets/home/slide-3.jpg";
+import img4 from "./../../../assets/home/slide-4.jpg";
+import img5 from "./../../../assets/home/slide-5.jpg";
+import img1 from "./../../../assets/home/slide1.jpg";
 
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import "./Categories.css";
+
+import { Autoplay } from "swiper";
+import SectionTitle from "../../Shared/SectionTitle";
+import SingleCategories from "./SingleCategories";
+
+SwiperCore.use([EffectCoverflow, Pagination]);
+
+//create slider data
+const categoriesSlide = [
+  {
+    id: 1,
+    name: "Salads",
+    img: img1,
+  },
+  {
+    id: 2,
+    name: "Soups",
+    img: img2,
+  },
+  {
+    id: 3,
+    name: "Pizzas",
+    img: img3,
+  },
+  {
+    id: 4,
+    name: "Desserts",
+    img: img4,
+  },
+  {
+    id: 5,
+    name: "Desserts",
+    img: img5,
+  },
+];
 
 const Categories = () => {
-    return (
-        <div>
-            <h1>this is Categories Page</h1>
-        </div>
-    );
+  return (
+    <section className="my-24">
+      <SectionTitle
+        subheading="---From 11:00am to 10:00pm---"
+        heading="ORDER ONLINE"
+      />
+      <div>
+        <Swiper
+          effect={"coverflow"}
+          grabCursor={true}
+          centeredSlides={true}
+          loop={true}
+          autoplay={{ delay: 2000, disableOnInteraction: true }}
+          slidesPerView={"auto"}
+          modules={[Autoplay]}
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+          }}
+          pagination={true}
+          breakpoints={{
+            640: {
+              slidesPerView: 1,
+              spaceBetween: 2,
+            },
+            768: {
+              slidesPerView: 2,
+              spaceBetween: 10,
+            },
+            1024: {
+              slidesPerView: 3,
+              spaceBetween: 10,
+            },
+          }}
+        >
+          {categoriesSlide.map((category) => {
+            return (
+              <SwiperSlide key={category.id}>
+                <SingleCategories key={category.id} category={category} />
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      </div>
+    </section>
+  );
 };
 
 export default Categories;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../../components/Home/Banner/Banner";
+import Categories from "../../components/Home/Categories/Categories";
 
 
 const Home = () => {
     return (
         <>
            <Banner/>
+           <Categories/>
         </>
     );
 };
